6448ab9d99bd73b91eddbcac975f65b8df44e22b
Abir Das <abir.das@mybos.com>
2023-04-15 12:48:11 +0600

    single api

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -22,7 +22,7 @@ class TransactionController extends Controller
         foreach ($inputData['transaction'] as $transaction) {
             $newTransaction = new Transaction();
             $newTransaction->date = $transaction['date'];
-            $newTransaction->descripton = $transaction['description'];
+            $newTransaction->description = $transaction['description'];
             $newTransaction->account_id = $transaction['account_id'];
             $newTransaction->debit_amount = $transaction['debit_amount'] ?? 0;
             $newTransaction->credit_amount = $transaction['credit_amount'] ?? 0;
@@ -34,7 +34,7 @@ class TransactionController extends Controller
 
             $newTransaction = new Transaction();
             $newTransaction->date = $transaction['date'];
-            $newTransaction->descripton = null;
+            $newTransaction->description = null;
             $newTransaction->account_id = $inputData['linked_id'];
             $newTransaction->debit_amount = $transaction['credit_amount'] ?? 0;
             $newTransaction->credit_amount = $transaction['debit_amount'] ?? 0;
@@ -55,5 +55,20 @@ class TransactionController extends Controller
             self::updateCategoryBalance($category['parent'], $balance);
         }
         return true;
+    }
+    public function single(Request $request)
+    {
+        $inputData = $request->all();
+        $validator = Validator::make($inputData, [
+            'id' => 'required'
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['status' => 500, 'errors' => $validator->errors()]);
+        }
+        $result = Transaction::select('id', 'date', 'account_id', 'debit_amount', 'credit_amount', 'description')
+            ->where('linked_id', $inputData['id'])
+            ->get()
+            ->toArray();
+        return response()->json(['status' => 200, 'data' => $result]);
     }
 }
