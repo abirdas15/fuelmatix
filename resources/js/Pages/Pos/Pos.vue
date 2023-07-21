b5f06bb6dd8115d0c03921ac2fc86cb5e0f6a06f
Khalid Imran <ekhalid090@gmail.com>
2023-07-21 13:51:45 +0600

    [pos

--- resources/js/Pages/Pos/Pos.vue
+++ resources/js/Pages/Pos/Pos.vue
@@ -45,39 +45,50 @@
                                             <th class="text-end"></th>
                                         </tr>
                                         </thead>
-                                        <tbody>
-                                        <tr>
+                                        <tbody v-if="sale.length > 0">
+                                        <tr v-for="(s, i) in sale">
                                             <td>
-                                                <div class="fw-bold">T-shirt</div>
+                                                <div class="fw-bold">{{s.name}}</div>
                                                 <div>
-                                                    <span class="badge badge-primary">Primary</span>
-                                                    <i class="fa-solid fa-pen"></i>
+                                                    <span class="badge badge-primary">{{ s.type }}</span>
                                                 </div>
                                             </td>
                                             <td>
                                                 <div class="d-flex align-items-center justify-content-between">
-                                                    <div class="btn-cart-plus">-</div>
-                                                    <div>2</div>
-                                                    <div class="btn-cart-plus">+</div>
+                                                    <div class="btn-cart-plus cursor-pointer" @click="updateProduct('minus', i)">-</div>
+                                                    <input class="form-control control-sm" step='0.01' type="number" v-model="parseFloat(s.quantity).toFixed(2)" @input="updateSubtotal(i)">
+                                                    <div class="btn-cart-plus cursor-pointer" @click="updateProduct('plus', i)">+</div>
                                                 </div>
                                             </td>
                                             <td class="text-end">
-                                                $ 20.00
+                                                $ {{ s.price }}
                                             </td>
                                             <td class="text-end">
-                                                $ 40.00
+                                                <input class="form-control w-100 control-sm text-end" step="any" type="number" v-model="s.subtotal" @input="updateQuantity(i)">
                                             </td>
                                             <td class="text-end">
-                                                <i class="fa-regular text-danger fa-trash-can"></i>
+                                                <i class="fa-regular text-danger fa-trash-can cursor-pointer" @click="removeProduct(i)"></i>
                                             </td>
+                                        </tr>
+                                        <tr>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td class="text-end">Total: $ <strong>{{getProductTotalPrice()}}</strong></td>
+                                        </tr>
+                                        </tbody>
+                                        <tbody v-if="sale.length === 0">
+                                        <tr class="text-center">
+                                            <td colspan="20">Please add product</td>
                                         </tr>
                                         </tbody>
                                     </table>
                                 </div>
                                 <div class="btn-section text-center mt-3">
                                     <button class="btn btn-warning me-2 width-fixed">Hold <i class="fa-regular fa-hand"></i></button>
-                                    <button class="btn btn-danger me-2 width-fixed">Reset <i class="fa-solid fa-arrow-rotate-left"></i></button>
-                                    <button class="btn btn-success width-fixed">Paynow <i class="fa-solid fa-money-bill-1"></i></button>
+                                    <button class="btn btn-danger me-2 width-fixed" @click="this.sale = []">Reset <i class="fa-solid fa-arrow-rotate-left"></i></button>
+                                    <button class="btn btn-success width-fixed" v-if="!loading" @click="order">Paynow <i class="fa-solid fa-money-bill-1"></i></button>
+                                    <button class="btn btn-success width-fixed" v-if="loading" @click="order">Paying.... <i class="fa fa-spinner fa-spin"></i></button>
                                 </div>
                             </div>
                         </div>
@@ -94,22 +105,24 @@
                             </div>
                             <div class="default-cart">
                                 <div class="d-flex flex-nowrap overflow-auto pb-2 mb-2">
-                                    <button class="btn btn-sm me-2" :class="{'btn-active': selectedProductIndex == undefined}" @click="getProducts">All Categories</button>
-                                    <button class="btn btn-sm light btn-dark me-2" v-for="(type, i) in productType" :class="{'btn-active': i == selectedProductIndex}" @click="getProducts(type.id)">{{ type.name }}</button>
+                                    <button class="btn btn-sm light btn-dark me-2" :class="{'active-btn': selectedProductIndex == undefined}" @click="getProducts()">All Categories</button>
+                                    <button class="btn btn-sm light btn-dark me-2" v-for="(type, i) in productType" :class="{'active-btn': i == selectedProductIndex}" @click="getProducts(type.id, i)">{{ type.name }}</button>
                                 </div>
                                 <div class="product-list">
-                                    <div class="each-product" v-for="(p, i) in products">
+                                    <div class="each-product" v-for="(p, i) in products" @click="cartProduct(p)">
                                         <div class="img">
                                             <img :src="'https://via.placeholder.com/100x70?text='+p.name" alt="">
                                         </div>
                                         <div class="detail">
                                             <div class="name">{{p.name}}</div>
-                                            <div class="desc">{{ p.product_type }}</div>
-                                            <p class="mt-1 mb-0"><kbd>Alt</kbd>+<kbd>{{getProductNumber(i)}}</kbd></p>
+                                            <div class="d-flex align-items-center justify-content-between">
+                                                <div class="desc">{{ p.product_type }}</div>
+                                                <div>$ {{p.selling_price}}</div>
+                                            </div>
+                                            <p class="mt-1 mb-0" v-if="i < 9"><kbd>Alt</kbd>+<kbd>{{getProductNumber(i)}}</kbd></p>
                                         </div>
                                     </div>
                                 </div>
-
                             </div>
                         </div>
                     </div>
@@ -122,37 +135,129 @@
 <script>
 import ApiService from "../../Services/ApiService";
 import ApiRoutes from "../../Services/ApiRoutes";
-import {active} from "../../../../public/js/app";
 
 export default {
     data() {
         return {
+            sale: [],
             products: [],
             productType: [],
-            selectedProductIndex: null
+            selectedProductIndex: null,
+            saleId: null,
+            loading: false
         }
     },
     methods: {
-        active,
-        getProducts: function (id = null) {
+        updateSubtotal: function (i) {
+            this.sale[i].subtotal = this.sale[i].price * this.sale[i].quantity
+        },
+        updateQuantity: function (i) {
+            this.sale[i].quantity = this.sale[i].subtotal / this.sale[i].price
+        },
+        order: function () {
+            this.loading = true
+            ApiService.POST(ApiRoutes.SaleAdd, this.sale,res => {
+                this.loading = false
+                if (parseInt(res.status) === 200) {
+                    this.saleId = res.data
+                }
+            });
+        },
+        getProductTotalPrice: function () {
+            let total = 0
+            this.sale.map(v => {
+                total += parseFloat(v.subtotal)
+            })
+            if (isNaN(total)) {
+                return 0
+            }
+            return total
+        },
+        updateProduct: function (type, i) {
+            if (type == 'minus') {
+                if (this.sale[i].quantity == 1) {
+                    this.sale.splice(i, 1)
+                } else {
+                    this.sale[i].quantity--
+                    this.sale[i].subtotal = this.sale[i].quantity * this.sale[i].price
+                }
+            }
+            if (type == 'plus') {
+                this.sale[i].quantity++
+                this.sale[i].subtotal = this.sale[i].quantity * this.sale[i].price
+            }
+        },
+        removeProduct: function (i) {
+            this.sale.splice(i, 1)
+        },
+        cartProduct: function (p) {
+            let product = {
+                name: p.name,
+                type: p.product_type,
+                product_id: p.id,
+                quantity: 1.00,
+                price: p.selling_price,
+                subtotal: p.selling_price,
+            }
+            let isExist = this.sale.map(v => v.product_id).indexOf(product.product_id);
+            if (isExist > -1) {
+                this.updateProduct('plus', isExist)
+            } else {
+                this.sale.push(product)
+            }
+        },
+        getProducts: function (id = null, index =  null) {
+            this.selectedProductIndex = index
             let param = {
                 limit: 5000,
                 page: 1
             }
             if (id != null) {
-                this.selectedProductIndex = id
                 param.type_id = id;
             }
             ApiService.POST(ApiRoutes.ProductList, param, res => {
                 if (parseInt(res.status) === 200) {
                     this.products = res.data.data;
-                    document.addEventListener("keydown", function (event) {
+                    document.addEventListener("keydown", (event) => {
                         if (event.altKey) {
                             event.stopPropagation();
                             event.preventDefault();
                         }
                         if(event.altKey && event.key == 'a') {
-                            console.log("CTRL + a was pressed!");
+                            let product = this.products[0]
+                            this.cartProduct(product)
+                        }
+                        if(event.altKey && event.key == 's') {
+                            let product = this.products[1]
+                            this.cartProduct(product)
+                        }
+                        if(event.altKey && event.key == 'd') {
+                            let product = this.products[2]
+                            this.cartProduct(product)
+                        }
+                        if(event.altKey && event.key == 'f') {
+                            let product = this.products[3]
+                            this.cartProduct(product)
+                        }
+                        if(event.altKey && event.key == 'g') {
+                            let product = this.products[4]
+                            this.cartProduct(product)
+                        }
+                        if(event.altKey && event.key == 'h') {
+                            let product = this.products[5]
+                            this.cartProduct(product)
+                        }
+                        if(event.altKey && event.key == 'j') {
+                            let product = this.products[6]
+                            this.cartProduct(product)
+                        }
+                        if(event.altKey && event.key == 'k') {
+                            let product = this.products[7]
+                            this.cartProduct(product)
+                        }
+                        if(event.altKey && event.key == 'l') {
+                            let product = this.products[8]
+                            this.cartProduct(product)
                         }
                     });
                 }
@@ -180,15 +285,12 @@ export default {
                     alphabet = 'H'
                     break;
                 case 6:
-                    alphabet = 'I'
-                    break;
-                case 7:
                     alphabet = 'J'
                     break;
-                case 8:
+                case 7:
                     alphabet = 'K'
                     break;
-                case 9:
+                case 8:
                     alphabet = 'L'
                     break;
                 default:
@@ -220,6 +322,7 @@ export default {
     align-items: center;
     flex-wrap: wrap;
     .each-product{
+        cursor: pointer;
         border: 1px solid #f2f2f2;
         background-color: #ffffff;
         margin-right: 15px;
@@ -291,5 +394,12 @@ export default {
     background-color: #6572FF;
     border-color: #6572FF;
     color: #ffffff;
+}
+.control-sm{
+    padding: 8px 10px;
+    width: 100px;
+    margin-left: 10px;
+    margin-right: 10px;
+    height: 2.5rem;
 }
 </style>
